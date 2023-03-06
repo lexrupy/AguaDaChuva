54eac0e2196ed2bf0ec6a3caf6788dc9e4863a9c
Alexandre da Silva <lexrupy@gmail.com>
2023-03-06 11:37:47 -0300

    WIP adicionar Rotary Encoder e Menus

--- AguaDaChuva.ino
+++ AguaDaChuva.ino
@@ -24,6 +24,7 @@
 
 #include <EEPROM.h>
 #include <LiquidCrystal.h>
+#include <RotaryEncoder.h>
 
 
 #define RESERV_SENS A0
@@ -81,14 +82,26 @@
 // 2 -> iniciar quando nivel medio
 #define START_LEVEL 1
 
-LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_DT4, LCD_DT5, LCD_DT6, LCD_DT7);
+#define LEVEL_EMPTY 0
+#define LEVEL_LOW   1
+#define LEVEL_MID   2
+#define LEVEL_FULL  3
+#define HEART       4
+#define VOLTAR      5
 
-byte lvlEmpty[8]    = { 0x0E, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x1F };
-byte lvlLow[8]      = { 0x0E, 0x11, 0x11, 0x11, 0x11, 0x1F, 0x1F, 0x1F };
-byte lvlMid[8]      = { 0x0E, 0x11, 0x11, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F };
-byte lvlFull[8]     = { 0x0E, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F };
-byte heartOpen[8]   = { 0x00, 0x0A, 0x15, 0x11, 0x11, 0x0A, 0x04, 0x00 }; // {	0b00000,	0b01010,	0b10101,	0b10001,	0b01010,	0b00100,	0b00000,	0b00000};
-byte heartClosed[8] = { 0x00, 0x0A, 0x1F, 0x1F, 0x1F, 0x0E, 0x04, 0x00 }; // {	0b00000,	0b01010,	0b11111,	0b11111,	0b01110,	0b00100,	0b00000,	0b00000};
+
+LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_DT4, LCD_DT5, LCD_DT6, LCD_DT7);
+RotaryEncoder encoder(ROTARY_ENCODER_DT, ROTARY_ENCODER_CLK, RotaryEncoder::LatchMode::TWO03);
+
+byte lvlEmpty[8] = { 0x0E, 0x11, 0x11, 0x11, 0x11, 0x11, 0x11, 0x1F };
+byte lvlLow[8]   = { 0x0E, 0x11, 0x11, 0x11, 0x11, 0x1F, 0x1F, 0x1F };
+byte lvlMid[8]   = { 0x0E, 0x11, 0x11, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F };
+byte lvlFull[8]  = { 0x0E, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F, 0x1F };
+byte heartChr[8] = { 0x00, 0x0A, 0x1F, 0x1F, 0x1F, 0x0E, 0x04, 0x00 };
+byte stateOn[8]  = { 0x00, 0x04, 0x0E, 0x1F, 0x1F, 0x0E, 0x04, 0x00 };//{ 0x0C, 0x12, 0x12, 0x0C, 0x12, 0x1A, 0x16, 0x12 };//{ 0x00, 0x0E, 0x15, 0x1F, 0x1F, 0x15, 0x0E, 0x00 };
+byte stateOff[8] = { 0x00, 0x04, 0x0A, 0x11, 0x11, 0x0A, 0x04, 0x00 };//{ 0x00, 0x0E, 0x11, 0x11, 0x11, 0x11, 0x0E, 0x00 };
+byte voltarChr[8]= { 0x04, 0x0C, 0x1F, 0x0D, 0x05, 0x01, 0x1F, 0x00 };
+
 
 
 unsigned long HEART_BEAT_LAST = 0;
@@ -126,12 +139,33 @@ int LAST_FLOW_MODE = -1; // Flow Mode: CONCES/CISTER
 
 
 int LCD_BL_STATE = 1; // 1 = Ligado, 0 = Desligado
-unsigned long LCD_BL_TIMEOUT = 5 * SECOND;//1 * MINUTE; // Default 1 minuto
+unsigned long LCD_BL_TIMEOUT = 1 * MINUTE; // Default 1 minuto
 int LCD_BL_INTENSIDADE = 50; // 50%
 unsigned long LAST_ITERATION_TIME = 0;
 
 bool CISTER_STARTUP_DONE = false;
 bool RESERV_STARTUP_DONE = false;
+
+// ================================ menu ==================================
+int ULTIMO_MENU = 0;
+int ULTIMO_SUBMENU = 0;
+int MENU_ATUAL = 0;
+int SUBMENU_ATUAL = 0;
+bool IN_MENU = false;
+bool IN_SUBMENU = false;
+
+bool DO_SUBMENU_ACTION = false;
+
+int MENU_SIZE = 5;
+int SUBMENU_SIZE = 2;
+
+bool DO_MENU_DRAW = false;
+
+
+int ENCODER_POS = 0;
+unsigned long LAST_ENCODER_SW_TIME;
+unsigned long ENCODER_SW_DEBOUNCE_TIME;
+int LAST_ENCODER_SW_STATE = 0;
 
 
 
@@ -168,23 +202,40 @@ void setup() {
   lcd.print("Inicializando...");
   delay(STARTUP_TIME);
   lcd.clear();
-  lcd.createChar(0, lvlEmpty);
-  lcd.createChar(1, lvlLow);
-  lcd.createChar(2, lvlMid);
-  lcd.createChar(3, lvlFull);
-  lcd.createChar(4, heartOpen);
-  lcd.createChar(5, heartClosed);
+  lcd.createChar(LEVEL_EMPTY, lvlEmpty);
+  lcd.createChar(LEVEL_LOW, lvlLow);
+  lcd.createChar(LEVEL_MID, lvlMid);
+  lcd.createChar(LEVEL_FULL, lvlFull);
+  lcd.createChar(HEART, heartChr);
+  lcd.createChar(VOLTAR, voltarChr);
   printStatusSkel();
 
   Serial.begin(9600);
+
+  PCICR |= (1 << PCIE1);
+  PCMSK1 |= (1 << PCINT10) | (1 << PCINT11);
+}
+
+ISR(PCINT1_vect) {
+  encoder.tick(); // just call tick() to check the state.
 }
 
 void loop() {
   LOOP_TIME = millis();
+  readMenuSwitch();
   readCisternStatus();
   readReservatoryStatus();
+  // Checagem de segurança para desligar apos certo tempo sem concluir o enchimento da caixa
   doSecurityCheck();
-  if (CISTER_STARTUP_DONE && RESERV_STARTUP_DONE) {
+  // Controle do Fluxo de Agua, motor, solenoide ETC
+  controleDeFluxo();
+  updateLCD();
+  heartBeat();
+  checkBacklight();
+}
+
+void controleDeFluxo(){
+    if (CISTER_STARTUP_DONE && RESERV_STARTUP_DONE) {
     if (RESERV_EMPTY_STATE) { /* CASO O RESERVATORIO NECESSITE ENCHER */
       if (!CISTER_EMPTY_STATE) { /* SE HOUVER AGUA NA CISTERNA ACIONA O MOTOR DA CISTERNA */
         if (CISTER_FLOW_STATUS == 0 && CISTER_FLOW_ERROR == 0) {
@@ -207,23 +258,98 @@ void loop() {
       solenoidOff();
     }
   }
+}
 
-  updateLCD();
-  heartBeat();
-  checkBacklight();
+void readMenuSwitch(){
+  int newPos = encoder.getPosition();
+  int direction = 0;
+  if (newPos != ENCODER_POS) {
+    if (newPos > ENCODER_POS) direction = 1;
+    LAST_ITERATION_TIME = LOOP_TIME;
+    ENCODER_POS = newPos;
+    if (LCD_BL_STATE == 1) {
+      if (processEncoderRotation(direction) == 1) DO_MENU_DRAW = true;
+    }
+  }
+  
+  int encoderSWState = digitalRead(ROTARY_ENCODER_SW);
+
+  if ( (LOOP_TIME - LAST_ENCODER_SW_TIME) > ENCODER_SW_DEBOUNCE_TIME) {
+    if ((encoderSWState != LAST_ENCODER_SW_STATE) && (encoderSWState == LOW)) {
+      LAST_ITERATION_TIME = LOOP_TIME;
+      // Apenas executa Iteração se o Backlight Estiver Ligado, senão apenas atualiza o tempo de ultima
+      // iteracao para o lcd acender
+      if (LCD_BL_STATE == 1) {
+        processEncoderButtonPress();
+        DO_MENU_DRAW = true;
+      }
+    }
+    LAST_ENCODER_SW_STATE = encoderSWState;
+    ENCODER_SW_DEBOUNCE_TIME = LOOP_TIME;
+  }
+}
+
+int processEncoderRotation(int direction) {
+  int result = 0;
+  if (IN_MENU) {
+    // NO SUBMENU
+    if (IN_SUBMENU) {
+      ULTIMO_SUBMENU = SUBMENU_ATUAL;
+      if (direction == 1) {
+        if (SUBMENU_ATUAL < SUBMENU_SIZE) {
+          SUBMENU_ATUAL++;
+          result = 1;
+        }
+      } else {
+        if (SUBMENU_ATUAL > 0) {
+          SUBMENU_ATUAL--;
+          result = 1;
+        }
+      }
+    // NO MENU PRINCIPAL
+    } else {
+      ULTIMO_MENU = MENU_ATUAL;
+      if (direction == 1) {
+        if (MENU_ATUAL < MENU_SIZE) {
+          MENU_ATUAL++;
+          result = 1;
+        }
+      } else {
+        if (MENU_ATUAL > 0) {
+          MENU_ATUAL--;
+          result = 1;
+        }
+      }
+    }
+  }
+  return result;
+}
+
+void processEncoderButtonPress() {
+  if (!IN_MENU) {
+    MENU_ATUAL = 0;
+    IN_MENU = true;
+  } else {
+    if (!IN_SUBMENU) {
+      SUBMENU_ATUAL = 0;
+      IN_SUBMENU = true;
+    } else {
+      DO_SUBMENU_ACTION = true;
+    }
+  }
 }
 
 void checkBacklight() {
   // Backlight
-  // if (LOOP_TIME - LAST_ITERATION_TIME > LCD_BL_TIMEOUT) {
-  //   if (LCD_BL_STATE == 1) {
-  //     lcdBackLightOff();
-  //   }
-  // } else {
-  //   if (LCD_BL_STATE == 0) {
-  //     lcdBackLightOn();
-  //   }
-  // }
+  if (LOOP_TIME - LAST_ITERATION_TIME > LCD_BL_TIMEOUT) {
+    if (LCD_BL_STATE == 1) {
+      lcdBackLightOff();
+    }
+  } else {
+    if (LCD_BL_STATE == 0) {
+      lcdBackLightOn();
+    }
+  }
 }
 
 void lcdBackLightOff() {
@@ -304,7 +430,7 @@ void doSecurityCheck() {
 void printStatusSkel() {
   lcd.setCursor(0, 0);
   lcd.print("Cx:  Ct:  AUTO ");
-  lcd.write(byte(4));
+  lcd.write(byte(HEART));
   lcd.setCursor(0, 1);
   lcd.print("Ocioso     --:--");
   if (CONCES_FLOW_ERROR == 1 | CISTER_FLOW_ERROR == 1) {
@@ -317,7 +443,109 @@ void printStatusSkel() {
   }
 }
 
+void printMenu5(){}
+void printMenu4(){}
+void printMenu3(){}
+void printMenu2(){}
+
+void printMenu1() {
+  lcd.setCursor(0,0);
+  //         1234567890123456
+  lcd.print("< Estado Motor >");
+
+  if (IN_SUBMENU) {
+    lcd.setCursor(0,1);
+    //         1234567890123456
+    lcd.print("  ON     OFF   ");
+    lcd.write(byte(VOLTAR));
+    if (CISTER_FLOW_STATUS == 1) {
+      lcd.setCursor(1,1);
+      lcd.print("*");
+      lcd.setCursor(8,1);
+      lcd.print(" ");
+    } else {
+      lcd.setCursor(1,1);
+      lcd.print(" ");
+      lcd.setCursor(8,1);
+      lcd.print("*");
+    }
+    switch (SUBMENU_ATUAL) {
+      case 0:
+        lcd.setCursor(0,1);
+        lcd.print(">");
+        lcd.setCursor(7,1);
+        lcd.print(" ");
+        lcd.setCursor(14,1);
+        lcd.print(" ");
+        break;
+      case 1:
+        lcd.setCursor(0,1);
+        lcd.print(" ");
+        lcd.setCursor(7,1);
+        lcd.print(">");
+        lcd.setCursor(14,1);
+        lcd.print(" ");
+        break;
+      case 2:
+        lcd.setCursor(0,1);
+        lcd.print(" ");
+        lcd.setCursor(6,1);
+        lcd.print(" ");
+        lcd.setCursor(14,1);
+        lcd.print(">");
+        break;
+    }
+  } else {
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+  }
+}
+
+void printMenu0() {
+  lcd.setCursor(0,0);
+  //         1234567890123456
+  lcd.print("   Modo Oper.  >");
+  if (IN_SUBMENU) {
+    lcd.setCursor(0,1);
+    lcd.print(" * Automatico  >");
+  } else {
+    lcd.setCursor(0,1);
+    lcd.print("                ");
+  }
+}
+
 void updateLCD() {
+  if (!IN_MENU) {
+    statusScreen();    
+  } else {
+    if (DO_MENU_DRAW) {
+      switch (MENU_ATUAL) {
+        case 0:
+          printMenu0();
+          break;
+        case 1:
+          printMenu1();
+          break;
+        case 2:
+          printMenu2();
+          break;
+        case 3:
+          printMenu3();
+          break;
+        case 4:
+          printMenu4();
+          break;
+        case 5:
+          printMenu5();
+          break;
+      }
+    }
+
+  }
+  DO_MENU_DRAW = false;
+}
+
+void statusScreen() {
   if (FLOW_STATUS_CHANGED) {
     printStatusSkel();
     if (LAST_FLOW_MODE == FLOW_CISTER) {
@@ -340,16 +568,16 @@ void updateLCD() {
   lcd.setCursor(8, 0);
   switch (CISTER_LEVEL_READ) {
     case 0:
-      lcd.write(byte(0));
+      lcd.write(byte(LEVEL_EMPTY));
       break;
     case 1:
-      lcd.write(byte(1));
+      lcd.write(byte(LEVEL_LOW));
       break;
     case 2:
-      lcd.write(byte(2));
+      lcd.write(byte(LEVEL_MID));
       break;
     case 3:
-      lcd.write(byte(3));
+      lcd.write(byte(LEVEL_FULL));
       break;
   }
 
@@ -357,22 +585,22 @@ void updateLCD() {
   lcd.setCursor(3, 0);
   switch (RESERV_LEVEL_READ) {
     case 0:
-      lcd.write(byte(0));
+      lcd.write(byte(LEVEL_EMPTY));
       break;
     case 1:
-      lcd.write(byte(1));
+      lcd.write(byte(LEVEL_LOW));
       break;
     case 2:
-      lcd.write(byte(2));
+      lcd.write(byte(LEVEL_MID));
       break;
     case 3:
-      lcd.write(byte(3));
+      lcd.write(byte(LEVEL_FULL));
       break;
   }
 
   if (CONCES_FLOW_STATUS == 1) {
     lcd.setCursor(0, 1);
-               
+              
     lcd.print("Solen. On  ");
     printTime(TOTAL_TIME_CONCES_FLOW);
   }
@@ -389,11 +617,11 @@ void readCisternStatus() {
   // CHEIA ~1023
   if (SENSOR_VALUE > 800) {
     LEVEL_READ = 3;
-  } else if (SENSOR_VALUE < 800 && SENSOR_VALUE > 565) {
-    // MEIA ~613
+  } else if (SENSOR_VALUE < 700 && SENSOR_VALUE > 450) {
+    // MEIA ~508
     LEVEL_READ = 2;
-  } else if (SENSOR_VALUE < 565 && SENSOR_VALUE > 400) {
-    // BAIXA ~517
+  } else if (SENSOR_VALUE < 450 && SENSOR_VALUE > 250) {
+    // BAIXA ~337
     LEVEL_READ = 1;
   } else {
     // VAZIA ~0;
@@ -460,15 +688,17 @@ void readReservatoryStatus() {
 }
 
 void heartBeat() {
-  if (LOOP_TIME - HEART_BEAT_LAST >= 1000) {
-    lcd.setCursor(15,0);
-    lcd.write(byte(5));
-  }
+  if (!IN_MENU) {
+    if (LOOP_TIME - HEART_BEAT_LAST >= 1000) {
+      lcd.setCursor(15,0);
+      lcd.write(byte(HEART));
+    }
 
-  if (LOOP_TIME - HEART_BEAT_LAST >= 1500) {
-    lcd.setCursor(15,0);
-    lcd.print(" ");
-    HEART_BEAT_LAST = LOOP_TIME;
+    if (LOOP_TIME - HEART_BEAT_LAST >= 1500) {
+      lcd.setCursor(15,0);
+      lcd.print(" ");
+      HEART_BEAT_LAST = LOOP_TIME;
+    }
   }
 }
 
